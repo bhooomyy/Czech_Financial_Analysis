d545aea0051558d948787ee1c89098a3d389001a
bhooomyy <bhattbhoomi0111@gmail.com>
2026-05-08 13:01:13 -0400

    update schema.sql
    - client.csv loaded into client table.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
 create database czechDB;
 use czechDB;
 CREATE TABLE client (client_id INT, birth_number INT, district_id INT);
+
+LOAD DATA INFILE '/Users/bhoomi/Downloads/BerkaCzechFinancialDataset/client.csv'
+INTO TABLE client 
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
