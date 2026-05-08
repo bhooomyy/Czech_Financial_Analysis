f9040b595f060c83a9fd0bd30e24ed33542b7804
bhooomyy <bhattbhoomi0111@gmail.com>
2026-05-08 15:57:00 -0400

    Update customerProfiling&Segmentation.sql
    - total number of customers per district

--- CustomerProfiling&Segmentation.sql
+++ CustomerProfiling&Segmentation.sql
@@ -1,2 +1,5 @@
 -- Count total number of clients in the bank
 SELECT COUNT(DISTINCT client_id) FROM client;
+
+-- Count clients per district — which district has the most clients?
+SELECT d.district_id,d.district_name,COUNT(c.client_id) as client_cnt FROM district d INNER JOIN client c ON d.district_id=c.district_id GROUP BY d.district_id,d.district_name ORDER BY client_cnt DESC;
