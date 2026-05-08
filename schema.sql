c9a79c210268cd42d757ede130f875d16439b324
bhooomyy <bhattbhoomi0111@gmail.com>
2026-05-08 13:29:23 -0400

    Updated schema.sql
    - similarly card & account tables created.

--- schema.sql
+++ schema.sql
@@ -8,3 +8,19 @@ FIELDS TERMINATED BY ';'
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS;
+
+CREATE TABLE card (card_id INT, disp_id INT, type VARCHAR(25),issued varchar(50));
+LOAD DATA INFILE '/Users/bhoomi/Downloads/BerkaCzechFinancialDataset/card.csv'
+INTO TABLE card 
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
+
+CREATE TABLE account (account_id INT,district_id INT, frequency VARCHAR(30),date VARCHAR(7));
+LOAD DATA INFILE '/Users/bhoomi/Downloads/BerkaCzechFinancialDataset/account.csv'
+INTO TABLE account 
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
