5f4439cd3b8477ec20cc5256c8e51ba14d9cc94a
bhooomyy <bhattbhoomi0111@gmail.com>
2026-05-24 13:37:13 -0400

    Identify round-number transactions (amount divisible by 1000) — these are often suspicious. What % of all transactions are round numbers?

--- FraudSignals.sql
+++ FraudSignals.sql
@@ -6,3 +6,15 @@ SELECT
 	FROM trans
     GROUP BY account_id,per_day
     HAVING num_trans>5;
+
+-- Identify round-number transactions (amount divisible by 1000) — these are often suspicious. What % of all transactions are round numbers? 
+SELECT * FROM(SELECT
+	trans_id,
+    account_id,
+    amount,
+	(CASE WHEN amount%1000=0 THEN 'Suspicious' 
+		 ELSE 'Normal' 
+         END) AS flag_trans,
+    CONCAT(ROUND(SUM(CASE WHEN amount%1000=0 THEN 1 ELSE 0 END) OVER()*100.0/COUNT(*) OVER(),2),'%') AS flag_cnt
+	FROM trans) finding_flag 
+    WHERE flag_trans='Suspicious';
